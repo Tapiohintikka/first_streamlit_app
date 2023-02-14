38d6997cde9fb403d64dbed854281c49135a6322
Tapiohintikka <125366729+Tapiohintikka@users.noreply.github.com>
2023-02-14 14:22:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('breakfest menu')
+streamlit.text('Omega 3 blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket smoothie')
+streamlit.text('Hard boiled egg')
