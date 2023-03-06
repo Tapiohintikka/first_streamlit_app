324efc14137ffb601f9f06d15e25d412cbea0148
Tapiohintikka <125366729+Tapiohintikka@users.noreply.github.com>
2023-03-06 22:15:31 +0200

    Update streamlit_app.py
    Making the JSON Look Good

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,19 +9,22 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
 #new section to display fruitvice api responce
 streamlit.header('fruityvice fruit advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just writes data tot the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # take the json version of the responce and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
