5d66c10a5066132cdd90e6f2f6afefdca9fb2568
Tapiohintikka <125366729+Tapiohintikka@users.noreply.github.com>
2023-03-06 16:53:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruitvice_normalized
  
-    back_from_function = get_fruityvice_data(this_fruit_choice)
+    back_from_function = get_fruityvice_data(fruit_choice)
     back_from_function
     
   streamlit.error()
