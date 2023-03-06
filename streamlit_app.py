a2b511b96607b0fd44a2d7eabc22742c5105c2ae
Tapiohintikka <125366729+Tapiohintikka@users.noreply.github.com>
2023-03-06 22:12:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('breakfest menu')
 streamlit.text('🥣 Omega 3 blueberry Oatmeal')
@@ -7,25 +6,21 @@ streamlit.text('🥗 Kale, spinach & Rocket smoothie')
 streamlit.text('🐔 Hard boiled egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
 #new section to display fruitvice api responce
 streamlit.header('fruityvice fruit advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruitvice_response = requests.get("https://fruitvice.com/api/fruit/" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # just writes data tot the screen
 
 # take the json version of the responce and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
