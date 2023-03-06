9b5c59c66d516a4da5a55279910608564e59a77a
Tapiohintikka <125366729+Tapiohintikka@users.noreply.github.com>
2023-03-06 23:18:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,5 +61,4 @@ if streamlit.button('Get fruit load list'):
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
 
-#this will not work correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
