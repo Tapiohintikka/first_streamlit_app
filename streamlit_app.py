6746ec43ca70b12e6ae856e5e7789f197099538c
Tapiohintikka <125366729+Tapiohintikka@users.noreply.github.com>
2023-03-06 15:49:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,6 @@ try:
 except URLEerror as e:
   streamlit.error()
 
-    streamlit.stop()
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
