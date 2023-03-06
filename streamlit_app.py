c1d60b0e88cdbfa4e245434dcf220a5c512d18cf
Tapiohintikka <125366729+Tapiohintikka@users.noreply.github.com>
2023-03-06 18:46:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit
-import pandas
-import requests
-import snowflake.connector
 
-from urllib.error import URLError
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('breakfest menu')
 streamlit.text('🥣 Omega 3 blueberry Oatmeal')
@@ -12,43 +8,16 @@ streamlit.text('🐔 Hard boiled egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:  
-    streamlit.error("please select a fruit to get information")
-  else: 
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-except URLEerror as e:
-  streamlit.error()
 
-
-streamlit.header("The fruit load list contains:")
-#snowflake-related functions
-def get_fruit_list():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from fruit_load_list")
-    return my_cur.fetchall()
-  
-#add a button to load data the fruit
-if streamlit.button('get fruit from list'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)
-
-# adding new fruit
-streamlit.header("What fruit you like to add?")
-Add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
-streamlit.write('Thanks for adding', Add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-streamlit.stop()
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
